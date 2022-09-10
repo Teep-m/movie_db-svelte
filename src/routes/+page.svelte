6b6326ef84fc02f9a160c0afbdd5ddc392539f2d
Teep-m <103185983+Teep-m@users.noreply.github.com>
2022-09-10 22:53:17 +0900

    'first commit'

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<button>Click here</button>
+<a href="/about">Go to About</a>
+
+<style>
+	button {
+		cursor: pointer;
+		background-color: black;
+		color: white;
+		border-radius: 30px;
+	}
+
+	p a {
+		color: blue;
+	}
+</style>
